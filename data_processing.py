0a7b06b8d6592ea2a791c37bb9d317d4168e2c6c
selwin-george <50783754+selwin-george@users.noreply.github.com>
2022-04-30 16:24:48 -0700

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Reading in sentiment140 dataset
 df = pd.read_csv("training.1600000.processed.noemoticon.csv",
                 names=['polarity', 'id', 'date', 'query', 'user', 'text'],
                 encoding='latin-1')
@@ -8,4 +9,5 @@ tweet_sentiment_pairs = df[['text','polarity']]
 tweet_sentiment_pairs['polarity'] //= 4
 tweet_sentiment_pairs.rename(columns = {'polarity':'sentiment'}, inplace = True)
 
+# Write csv of text, sentiment pairs (0 negative, 1 positive)
 tweet_sentiment_pairs.to_csv('tweet_sentiment_pairs.csv')
